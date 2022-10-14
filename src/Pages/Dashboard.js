a81c1bcb6baee96e34884b8f863f310fdc3c7c0e
Parker <parkerarchibald97@gmail.com>
2022-10-14 07:43:43 -0600

    live counts for dashboard quotes and orders

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import '../Styles/Dashboard.css';
 import {Link} from 'react-router-dom';
+import {RELIE_API} from '../Config/Com';
 
 const Dashboard = () => {
+
+    const [state, setState] = useState({
+        quoteCount: 0,
+        orderCount: 0
+    });
+
+    useEffect(() => {
+        getQuoteCount();
+    }, [])
+
+    async function getQuoteCount() {
+        fetch(`${RELIE_API}quotes&OrdersNumber/${localStorage.getItem('id')}`)
+        .then(results => results.json())
+        .then(results => setState({quoteCount: Object.values(results[0])[0], orderCount: Object.values(results[1])[0]}))
+    }
+
     return (
         <div id='dashboardContainer'>
             <div id='dashboardTitle'>Dashboard</div>
@@ -13,14 +30,14 @@ const Dashboard = () => {
                 </Link>
             </div>
             <div id='dashboardData'>
-                <div id='dashboardQuotesBox'>
+                <Link to='/quotes' id='dashboardQuotesBox' style={{'textDecoration': 'none', 'color': 'black'}}>
                     <div id='dashboardQuotesTitle'>My Quotes</div>
-                    <div id='dashBoardQuotesAmount'>45</div>
-                </div>
-                <div id='dashboardOrdersBox'>
+                    <div id='dashBoardQuotesAmount'>{state.quoteCount}</div>
+                </Link>
+                <Link to='/orders' id='dashboardOrdersBox' style={{'textDecoration': 'none', 'color': 'black'}}>
                     <div id='dashboardOrdersTitle'>My Orders</div>
-                    <div id='dashBoardOrderAmount'>20</div>
-                </div>
+                    <div id='dashBoardOrderAmount'>{state.orderCount}</div>
+                </Link>
             </div>
         </div>
     )
